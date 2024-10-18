d5de06a8058eaa0e96481e0d998d4cd45c4bc4bc
Aleksandr <alex.kharchenko@mail.ru>
2024-10-18 14:00:09 +0300

    Add files via upload

--- module_16_5/module_16_5.py
+++ module_16_5/module_16_5.py
@@ -1,1 +1,77 @@
+#uvicorn module_16_5:app --reload
 
+
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+from pydantic import BaseModel
+
+app = FastAPI()
+templates = Jinja2Templates(directory='templates')
+users = []
+
+
+class User(BaseModel):
+    id: int
+    username: str
+    age: int
+
+
+@app.get('/')
+async def get_users(request: Request):
+    return templates.TemplateResponse('users.html', {'request': request, 'users': users})
+
+
+@app.get('/users/{user_id}')
+async def user(request: Request, user_id: int):
+    for user in users:
+        if user.id == user_id:
+            return templates.TemplateResponse('users.html', {'request': request, "user": user})
+    raise HTTPException(status_code=404, detail="User was not found")
+
+
+@app.post("/user/{username}/{age}")
+async def create_user(username: str, age: int):
+    new_id = 1 if not users else users[-1].id + 1
+    new_user = User(id=new_id, username=username, age=age)
+    users.append(new_user)
+    return new_user
+
+
+@app.put("/user/{user_id}/{username}/{age}")
+async def update_user(user_id: int, username: str, age: int):
+    for user in users:
+        if user.id == user_id:
+            user.username = username
+            user.age = age
+            return user
+    raise HTTPException(status_code=404, detail="User was not found")
+
+
+@app.delete("/user/{user_id}")
+async def delete_user(user_id: int):
+    for i, user in enumerate(users):
+        if user.id == user_id:
+            deleted_user = users.pop(i)
+            return deleted_user
+    raise HTTPException(status_code=404, detail="User was not found")
+
+
+@app.put('/user/{user_id}/{username}/{age}')
+async def update_user(user_id: int, username: str, age: int):
+    for user in users:
+        if user.id == user_id:
+            user.username = username
+            user.age = age
+            return user
+    raise HTTPException(status_code=404, detail="User was not found")
+
+
+@app.delete("/user/{user_id}")
+async def delete_user(user_id: int):
+    for i, user in enumerate(users):
+        if user.id == user_id:
+            deleted_user = users.pop(i)
+            return deleted_user
+    raise HTTPException(status_code=404, detail="User was not found")
